905c8c2c94f6babc28bdd638c52d9131ecb17acf
ttttttboy@gmail.com <ttttttboy@gmail.com>
2019-01-28 21:25:46 +0800

    lib built

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 from GPSPhoto import gpsphoto
 
 def main():
-    orig_pic = '/Users/Leon/Desktop/IMG_0071.jpg'
+    orig_pic = '/Users/leon/Code/modPhotoGPS/data/IMG_0071.jpg'
+    print(os.path.exists(orig_pic))
     data = gpsphoto.getGPSData(orig_pic)
     rawdata = gpsphoto.getRawData(orig_pic)
     for tag in data.keys():
