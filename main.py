ad06379c6a035dff0eec3d6aeda27f28433b9353
Leon <ttttttboy@gmail.com>
2019-01-30 18:24:58 +0800

    test1

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import os
 from GPSPhoto import gpsphoto
 
 def main():
-    orig_pic = '/Users/leon/Code/modPhotoGPS/data/IMG_0071.jpg'
+    orig_pic = '/Users/Leon/Documents/project/modPhotoGPS/data/IMG_0752.jpeg'
     print(os.path.exists(orig_pic))
     data = gpsphoto.getGPSData(orig_pic)
     rawdata = gpsphoto.getRawData(orig_pic)
     for tag in data.keys():
         print("%s: %s" % (tag, data[tag]))
-
+    print("%s : %s" % (data['Latitude'], data['Longitude']))
     print("Hellps")
 
 
